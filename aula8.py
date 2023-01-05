4284b58a2ff6405e14af641cf2c4452dd6502bcb
victor <victor.correa@microdatasistemas.com.br>
2023-01-05 08:33:26 -0300

    adicionando aula 6 e 8

---
+++ aula8.py
@@ -0,0 +1,8 @@
+ 
+
+print ('nome:', nome)
+print ('sobrenome:', sobrenome)
+print ('idade:', idade)
+print ('Ano de nascimento:', ano_nascimento)
+print ('é maior de idade?', maior_de_idade)
+print ('Altura em metros:', altura_metros)
